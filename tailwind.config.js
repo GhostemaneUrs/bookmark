92d53e267c0064f567835756dcb302b7867754a2
Andres Jaramillo <JaramilloG1999@outlook.com>
2021-11-05 10:44:35 -0500

    feat(root): create tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+      padding: '16px',
+      screens: {
+        lg: '1124px',
+        xl: '1124px',
+        '2xl': '1124px',
+      }
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}

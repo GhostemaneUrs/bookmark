4526b8befd00051dc28540076ad2155903c80140
Andres Jaramillo <JaramilloG1999@outlook.com>
2021-11-05 10:46:54 -0500

    feat(root): using the fonts in the tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    fontFamily: {
+      Poppins: ['Poppins, sans-serif']
+    },
     container: {
       center: true,
       padding: '16px',

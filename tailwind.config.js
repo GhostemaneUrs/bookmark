82e33ee904863b73267919a9dbbb823ca57bd521
Andres Jaramillo <JaramilloG1999@outlook.com>
2021-11-06 00:58:36 -0500

    feat(root): update tailwind config adding colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'bookmard-red': '#FA5959',
+        'bookmard-blue': '#242A45',
+        'bookmard-grey': '#9194A2',
+        'bookmard-white': '#f7f7f7',
+        'bookmard-purple': '#5267DF'
+      }
+    },
     fontFamily: {
       Poppins: ['Poppins, sans-serif']
     },

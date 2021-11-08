dfaf9e3c3dd21cb3506c01d303d930d801e57b41
Andres Jaramillo <JaramilloG1999@outlook.com>
2021-11-07 21:42:10 -0500

    feat(root): update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,11 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'bookmard-red': '#FA5959',
-        'bookmard-blue': '#242A45',
-        'bookmard-grey': '#9194A2',
-        'bookmard-white': '#f7f7f7',
-        'bookmard-purple': '#5267DF'
+        'bookmark-red': '#FA5959',
+        'bookmark-blue': '#242A45',
+        'bookmark-grey': '#9194A2',
+        'bookmark-white': '#f7f7f7',
+        'bookmark-purple': '#5267DF'
       }
     },
     fontFamily: {
